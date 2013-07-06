7bb0a079a104d8b2d9df229aeabec1116dc5ac5c
Raoni F. Normanton <raonifn@gmail.com>
2013-07-05 22:13:54 -0300

    Update steam-track.js

--- steam-track.js
+++ steam-track.js
@@ -1,13 +1,15 @@
 (function($) {
-  var tracker = {]
-    url = 'http://steamcommunity.com/market/',
-    search = '/search?q='
+  var tracker = {
+    url : 'http://steamcommunity.com/market/',
+    search : '/search?q='
   };
   
-  funtion createIFrame() {
-    var iframe = #('<iframe width="100%"/>');
+  function createIFrame() {
+    var iframe = $('<iframe width="100%"/>');
     $('body').prepend(iframe);
     tracker.iframe = iframe;
-  }  
+  }
+  
+  createIFrame();
   
 })(jQuery);

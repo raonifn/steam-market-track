b04b353cc048aa4d708f76503a5bf72e8a3f4bc5
Raoni F. Normanton <raonifn@gmail.com>
2013-07-05 22:31:39 -0300

    Update steam-track.js

--- steam-track.js
+++ steam-track.js
@@ -5,11 +5,31 @@
   };
   
   function createIFrame() {
-    var iframe = $('<iframe width="100%"/>');
+    var iframe = $('<iframe width="100%" id="steam_track"/>');
     $('body').prepend(iframe);
     tracker.iframe = iframe;
   }
   
-  createIFrame();
+  function search(product) {
+    console.info("buscando", product);
+    var prodUrl = tracker.url + product;
+    tracker.iframe.attr('src', prodUrl);
+    tracker.iframe.ready(function() {
+      $.ajax({
+        url: prodUrl + '/render/?query=&start=1&count=100',
+        type: 'GET',
+        error: function(err) {
+          console.info('err', err);
+        },
+        success : function(data) {
+          console.info('got', data);
+        }
+      });
+    });
+  }
   
+  createIFrame();
+  search('/listings/753/SALVADOR');
+  
+  //http://steamcommunity.com/market/listings/753/SALVADOR/render/?query=&start=1&count=100
 })(jQuery);

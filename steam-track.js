25de3095b250d21c8a03293f51a0de0a8115cc6b
Raoni F. Normanton <raonifn@gmail.com>
2013-07-05 23:32:26 -0300

    Update steam-track.js

--- steam-track.js
+++ steam-track.js
@@ -4,49 +4,47 @@
     search : '/search?q='
   };
   
-  function createIFrame() {
-    var iframe = $('<iframe width="100%" id="steam_track"/>');
-    $('body').prepend(iframe);
-    tracker.iframe = iframe;
-  }
-  
   function search(product) {
-    console.info("buscando", product);
     var prodUrl = tracker.url + product;
-    //tracker.iframe.attr('src', prodUrl);
-    //tracker.iframe.ready(function() {
-      var query = prodUrl + '/render/?query=&start=1&count=100'
-      console.info('done', 'getting', query);
-      $.ajax({
-        url: query,
-        type: 'GET',
-        error: function(err) {
-          console.info('err', err);
-        },
-        success : function(data) {
-          console.info('ajax done, sucess', data.sucess);
-          handleHtml(data.results_html);
-        }
-      });
-//    });
+    var query = prodUrl + '/render/?query=&start=1&count=100'
+    console.info('done', 'getting', query);
+    $.ajax({
+      url: query,
+      type: 'GET',
+      error: function(err) {
+        console.info('err', err);
+      },
+      success : function(data) {
+        console.info('ajax done, sucess', data);
+        handleHtml(product, data.results_html);
+      }
+    });
   }
   
-  function handleHtml(html) {
+  function handleHtml(product, html) {
     var all = $(html);
     var spans = $(all).find('.market_listing_price_with_fee');
-    console.info('span', $(spans[0]).text(), $(spans[0]).val());
     var prices = [];
     spans.each(function(index) {
       var value = $(this).text();
       value = value.replace(/\s*.*(\d+[,\.]\d+)[^\d]*/m, "$1");
       value = value.replace(/,/, ".");
-      prices.push(value);
+      prices.push(parseFloat(value));
     });
     console.info('prices', prices);
+    
+    var avg = 0;
+    for (var i = 0; i < prices.length; i++) {
+      avg += prices[i];
+    }
+    avg /= prices.length;
+    
+    console.info('avg', avg);
+    console.info('min', prices[0]);
+    console.info('result', product, prices[0] / avg);
+    
   }
   
-  //createIFrame();
   search('/listings/753/SALVADOR');
-  
-  //http://steamcommunity.com/market/listings/753/SALVADOR/render/?query=&start=1&count=100
+  search('/listings/753/MEDIC%20%28Trading%20Card%29');
 })(jQuery);

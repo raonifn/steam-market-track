f3cc17519199e13953a0e598c7f2dc920319a932
Raoni Normanton <raonifn@gmail.com>
2013-07-08 13:29:12 -0300

    formatacao

--- steam-track.js
+++ steam-track.js
@@ -1,171 +1,170 @@
 (function($) {
-  var tracker = {
-    url : 'http://steamcommunity.com/market/',
-    search : '/search/render?start=0&count=1000&query=',
-    search_value : 'trading card',
-    minCount: 1000,
-    threshold: 0.8,
-    schedule_time: 15 * 1000,
-    use_alert: true,
-    alerts: []
-  };
-  
-  function updateParams() {
-    tracker.minCount = parseInt($('#minCount').val());
-    tracker.threshold = parseFloat($('#threshold').val());
-    tracker.schedule_time = parseInt($('#schedule_time').val());
-    tracker.search_value = $('#search_value').val();
-    tracker.use_alert = $('#use_alert').val();
-  }
-  
-  function stop() {
-    var started = tracker.timer;
-    if (started) {
-      console.info('parando');
-      clearInterval(tracker.timer);
-      delete tracker.timer;
-    }
-    return started;
-  }
-  
-  function start() {
-    console.info('agendado'); 
-    tracker.timer = setInterval(exec, tracker.schedule_time);       
-  }
-  
-  function init() {
-    var tracker_div = $('#tracker');
-    if (tracker_div) {
-      tracker_div.remove();
-    }
-    
-    var div = $('<div id="tracker" />');
-    div.attr('style', 'z-index: 100; width: 100%; height: 50%; position:absolute; top: 0; left: 0; ');
-    $('body').prepend(div);
-    
-    var button = $('<input id="toggle_schedule" type="button" value="toggle schedule"/>');
-    button.click(function() {
-      if (tracker.timer) {
-        stop();        
-      } else {
-        start();
-      }
-    });
+	var tracker = {
+		url : 'http://steamcommunity.com/market/',
+		search : '/search/render?start=0&count=1000&query=',
+		search_value : 'trading card',
+		minCount : 1000,
+		threshold : 0.8,
+		schedule_time : 15 * 1000,
+		use_alert : true,
+		alerts : []
+	};
 
-    div.append($('<input type="text" id="search_value" value="'+tracker.search_value+'" />'));
-    div.append($('<input type="text" id="minCount" value="'+tracker.minCount+'" />'));
-    div.append($('<input type="text" id="threshold" value="'+tracker.threshold+'" />'));
-    div.append($('<input type="text" id="schedule_time" value="'+tracker.schedule_time+'" />'));
-    div.append($('<input type="text" id="use_alert" value="true" />'));
-    var buttonUpdate = $('<input id="update_info" type="button" value="update data"/>');
-    buttonUpdate.click(function() {
-      var started = stop();
-      updateParams();
-      if (started) {
-        start();
-      }
-    });
+	function updateParams() {
+		tracker.minCount = parseInt($('#minCount').val());
+		tracker.threshold = parseFloat($('#threshold').val());
+		tracker.schedule_time = parseInt($('#schedule_time').val());
+		tracker.search_value = $('#search_value').val();
+		tracker.use_alert = $('#use_alert').val();
+	}
 
+	function stop() {
+		var started = tracker.timer;
+		if (started) {
+			console.info('parando');
+			clearInterval(tracker.timer);
+			delete tracker.timer;
+		}
+		return started;
+	}
 
-    div.append(buttonUpdate);
-    div.append(button);
+	function start() {
+		console.info('agendado');
+		tracker.timer = setInterval(exec, tracker.schedule_time);
+	}
 
-    if (tracker.alertInterval) {
-      clearInterval(tracker.alertInterval);
-    }
-    tracker.alertInterval = setInterval(do_alert, 10 * 1000);
-  }
-  
-  function list() {
-    var query = tracker.url + tracker.search + encodeURIComponent(tracker.search_value);
-    console.info('getting', query);
-    $.ajax({
-      url: query,
-      type: 'GET',
-      error: function(err) {
-        console.info('err', err);
-      },
-      success : function(data) {
-        console.info(data);
-        handleHtmlList(data.results_html);
-      }
-    });
-  }
-  
-  function handleHtmlList(data) {
-    var all = $(data);
-    var links = all.find('div.market_listing_row');
-    $(links).each(function(index) {
-      var lnk = $(this).parent().attr('href');
-      search(lnk);
-    });
-  }
-  
-  function search(product) {
-    var query = product + '/render/?query=&start=1&count=100'
-    $.ajax({
-      url: query,
-      type: 'GET',
-      error: function(err) {
-        console.info('err', err);
-      },
-      success : function(data) {
-        if (data.total_count > tracker.minCount) {
-          handleHtml(product, data);
-        }
-      }
-    });
-  }
-  
-  function handleHtml(product, data) {
-    var all = $(data.results_html);
-    var spans = $(all).find('.market_listing_price_with_fee');
-    var prices = [];
-    spans.each(function(index) {
-      var value = $(this).text();
-      value = value.replace(/\s*.*(\d+[,\.]\d+)[^\d]*/m, "$1");
-      value = value.replace(/,/, ".");
-      prices.push(parseFloat(value));
-    });
-    var avg = 0;
-    for (var i = 0; i < prices.length; i++) {
-      avg += prices[i];
-    }
-    avg /= prices.length;
-    
-    var result = prices[0] / avg;
-    if (result <= tracker.threshold) {
-      console.info('result', result, ', avg:',avg, 'min:', prices[0], 'product:', product, ' total:', data.total_count);
-      if (tracker.use_alert) {
-        tracker.alerts.push({
-          result: result,
-          avg: avg,
-          min: prices[0],
-          product: product,
-          total: data.total_count
-        });
-      }
-    }
-  }
-  
-  function do_alert() {
-    if (tracker.alerts && tracker.alerts.length > 0) {
-      var alerts = tracker.alerts;
-      tracker.alerts = [];
-      
-      var result = '';
-      for (var i = 0; i < alerts.length; i++) {
-        result += JSON.stringify(alerts[i]);
-      }
-      alert(result);
-    }
-  }
-  
-  
-  function exec() {
-    console.info('exec');
-    list();
-  }
-    
-  init();
+	function init() {
+		var tracker_div = $('#tracker');
+		if (tracker_div) {
+			tracker_div.remove();
+		}
+
+		var div = $('<div id="tracker" />');
+		div.attr('style', 'z-index: 100; width: 100%; height: 50%; position:absolute; top: 0; left: 0; ');
+		$('body').prepend(div);
+
+		var button = $('<input id="toggle_schedule" type="button" value="toggle schedule"/>');
+		button.click(function() {
+			if (tracker.timer) {
+				stop();
+			} else {
+				start();
+			}
+		});
+
+		div.append($('<input type="text" id="search_value" value="' + tracker.search_value + '" />'));
+		div.append($('<input type="text" id="minCount" value="' + tracker.minCount + '" />'));
+		div.append($('<input type="text" id="threshold" value="' + tracker.threshold + '" />'));
+		div.append($('<input type="text" id="schedule_time" value="' + tracker.schedule_time + '" />'));
+		div.append($('<input type="text" id="use_alert" value="true" />'));
+		var buttonUpdate = $('<input id="update_info" type="button" value="update data"/>');
+		buttonUpdate.click(function() {
+			var started = stop();
+			updateParams();
+			if (started) {
+				start();
+			}
+		});
+
+		div.append(buttonUpdate);
+		div.append(button);
+
+		if (tracker.alertInterval) {
+			clearInterval(tracker.alertInterval);
+		}
+		tracker.alertInterval = setInterval(do_alert, 10 * 1000);
+	}
+
+	function list() {
+		var query = tracker.url + tracker.search + encodeURIComponent(tracker.search_value);
+		console.info('getting', query);
+		$.ajax({
+			url : query,
+			type : 'GET',
+			error : function(err) {
+				console.info('err', err);
+			},
+			success : function(data) {
+				console.info(data);
+				handleHtmlList(data.results_html);
+			}
+		});
+	}
+
+	function handleHtmlList(data) {
+		var all = $(data);
+		var links = all.find('div.market_listing_row');
+		$(links).each(function(index) {
+			var lnk = $(this).parent().attr('href');
+			search(lnk);
+		});
+	}
+
+	function search(product) {
+		var query = product + '/render/?query=&start=1&count=100'
+		$.ajax({
+			url : query,
+			type : 'GET',
+			error : function(err) {
+				console.info('err', err);
+			},
+			success : function(data) {
+				if (data.total_count > tracker.minCount) {
+					handleHtml(product, data);
+				}
+			}
+		});
+	}
+
+	function handleHtml(product, data) {
+		var all = $(data.results_html);
+		var spans = $(all).find('.market_listing_price_with_fee');
+		var prices = [];
+		spans.each(function(index) {
+			var value = $(this).text();
+			value = value.replace(/\s*.*(\d+[,\.]\d+)[^\d]*/m, "$1");
+			value = value.replace(/,/, ".");
+			prices.push(parseFloat(value));
+		});
+		var avg = 0;
+		for ( var i = 0; i < prices.length; i++) {
+			avg += prices[i];
+		}
+		avg /= prices.length;
+
+		var result = prices[0] / avg;
+		if (result <= tracker.threshold) {
+			console.info('result', result, ', avg:', avg, 'min:', prices[0], 'product:', product, ' total:',
+					data.total_count);
+			if (tracker.use_alert) {
+				tracker.alerts.push({
+					result : result,
+					avg : avg,
+					min : prices[0],
+					product : product,
+					total : data.total_count
+				});
+			}
+		}
+	}
+
+	function do_alert() {
+		if (tracker.alerts && tracker.alerts.length > 0) {
+			var alerts = tracker.alerts;
+			tracker.alerts = [];
+
+			var result = '';
+			for ( var i = 0; i < alerts.length; i++) {
+				result += JSON.stringify(alerts[i]);
+			}
+			alert(result);
+		}
+	}
+
+	function exec() {
+		console.info('exec');
+		list();
+	}
+
+	init();
 })(jQuery);

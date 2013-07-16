eb7dc96d01e93a268e1cdad689eaac33dc3dd7b1
Raoni Normanton <raonifn@gmail.com>
2013-07-16 14:13:47 -0300

    test

--- steam-track.js
+++ steam-track.js
@@ -8,10 +8,19 @@
 		search_value : 'trading card',
 		debug : false,
 		stop : true,
-		minCount : 500,
-		threshold : 0.8,
-		schedule_time : 40 * 1000
+		minCount : 100,
+		threshold : 0.66,
+		schedule_time : 2 * 1000
 	};
+
+	var ajax_manager = {
+		queue : [],
+		mutex : 5
+	};
+
+	function ajax(opts) {
+		ajax_manager.queue.push(opts);
+	}
 
 	function updateParams() {
 		tracker.minCount = parseInt($('#minCount').val());
@@ -21,21 +30,50 @@
 	}
 
 	function stop() {
-		var started = tracker.timer;
+		var started = ajax_manager.timer;
 		if (started) {
 			console.info('parando');
-			clearInterval(tracker.timer);
-			delete tracker.timer;
+			clearInterval(ajax_manager.timer);
+			delete ajax_manager.timer;
 		}
 		return started;
 	}
 
+	function consume() {
+		if (ajax_manager.queue.length) {
+			if (ajax_manager.mutex > 0) {
+				ajax_manager.mutex--;
+				var opts = ajax_manager.queue.shift();
+				var success = opts.success;
+				var error = opts.error;
+				opts.error = function() {
+					try {
+						error(arguments);
+					} finally {
+						ajax_manager.mutex++;
+					}
+				}
+				opts.success = function() {
+					try {
+						success(arguments);
+					} finally {
+						ajax_manager.mutex++;
+					}
+				};
+			}
+		} else {
+			list();	
+		}
+	}
+
 	function start() {
 		console.info('agendado');
-		tracker.timer = setInterval(exec, tracker.schedule_time);
+		ajax_manager.timer = setInterval(exec, tracker.schedule_time);
 	}
 
 	function init() {
+		$.ajaxPrefilter(onAjax);
+
 		var tracker_div = $('#tracker');
 		if (tracker_div) {
 			tracker_div.remove();
@@ -53,7 +91,7 @@
 
 		var button = $('<input id="toggle_schedule" type="button" value="toggle schedule"/>');
 		button.click(function() {
-			if (tracker.timer) {
+			if (ajax_manager.timer) {
 				stop();
 			} else {
 				start();
@@ -88,7 +126,7 @@
 		var query = tracker.url + tracker.search + '?count=' + tracker.search_param.pagesize + '&start=' + start
 				+ '&query=' + encodeURIComponent(tracker.search_value);
 		console.info('getting', query);
-		$.ajax({
+		ajax({
 			url : query,
 			type : 'GET',
 			error : function(err) {
@@ -108,7 +146,7 @@
 	}
 
 	function handleHtmlList(data) {
-		if (!tracker.timer) {
+		if (!ajax_manager.timer) {
 			return;	
 		}
 		var all = $(data);
@@ -122,7 +160,7 @@
 
 	function search(product) {
 		var query = product + '/render/?query=&start=1&count=20'
-		$.ajax({
+		ajax({
 			url : query,
 			type : 'GET',
 			error : function(err) {
@@ -138,7 +176,7 @@
 	}
 
 	function handleHtml(product, data) {
-		if (!tracker.timer) {
+		if (!ajax_manager.timer) {
 			return;	
 		}
 		var all = $(data.results_html);
@@ -190,9 +228,13 @@
 		$('#tracker_messages').html('');
 	}
 
+	function onAjax(opts, originalOpts, jqXHR) {
+		
+	}
+
 	function exec() {
 		console.info('exec');
-		list();
+		consume();
 	}
 
 	init();

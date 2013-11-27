8089c1a3a4f888eb4a47eda6afc28b91ffc0c9d8
Raoni F. Normanton <raonifn@gmail.com>
2013-11-26 22:09:49 -0200

    Update steam-track.js

--- steam-track.js
+++ steam-track.js
@@ -13,7 +13,7 @@
 		threshold : 0.66,
 		schedule_time : 3 * 100,
 		minDiff: 0.1,
-		sessionid:  
+		sessionid: getCookie('sessionid')
 	};
 
 	var ajax_manager = {
@@ -22,9 +22,24 @@
 		mutex : 5
 	};
 
-	function getSessionid() {
-		sessionsesss
-   	}
+	function getCookie(c_name) {
+		ar c_value = document.cookie;
+		var c_start = c_value.indexOf(" " + c_name + "=");
+		if (c_start == -1) {
+		  c_start = c_value.indexOf(c_name + "=");
+		}
+		if (c_start == -1) {
+		  c_value = null;
+		} else {
+		  c_start = c_value.indexOf("=", c_start) + 1;
+		  var c_end = c_value.indexOf(";", c_start);
+		  if (c_end == -1) {
+			c_end = c_value.length;
+		  }
+		  c_value = unescape(c_value.substring(c_start,c_end));
+		}
+		return c_value;
+	}
 
 	function ajax(opts) {
 		ajax_manager.queue.push(opts);

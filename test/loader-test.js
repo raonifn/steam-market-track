6d6a366bf2346ac218f98b3b05d7587986c11971
Raoni Normanton <raonifn@gmail.com>
2014-04-07 15:41:18 -0300

    fixing

--- test/loader-test.js
+++ test/loader-test.js
@@ -14,3 +14,34 @@ asyncTest( "Test Loader", function() {
                        });
 
 });
+
+asyncTest( "Test Loader with prefix", function() {
+  var loader = new Loader('http://localhost:8084/test');
+
+  loader.load_sequence(['loader/loader1.js', 'loader/loader2.js'],
+                       function() {
+                         ok(LOADER1, 'loader1 ok');
+                         ok(LOADER2, 'loader2 ok');
+                         start();
+                       }, 
+                       function() {
+                         ok(false, 'fail to load');
+                         start();
+                       });
+
+});
+
+asyncTest( "Test Loader with fail", function() {
+  var loader = new Loader('http://localhost:8084/test');
+
+  loader.load_sequence(['loader/loader1.js', 'loader/loader3.js'],
+                       function() {
+                         ok(false, 'must fail cause loader3.js');
+                         start();
+                       }, 
+                       function() {
+                         ok(LOADER1, 'loader1 ok');
+                         start();
+                       });
+
+});

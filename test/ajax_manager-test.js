b8c3da9299b8b67f9d59599812145f71bc0befe6
Raoni Normanton <raonifn@gmail.com>
2014-04-08 09:17:49 -0300

    ajax_manager test

--- test/ajax_manager-test.js
+++ test/ajax_manager-test.js
@@ -1,4 +1,4 @@
-asyncTest( "Test ajax", function() {
+asyncTest( "Test start", function() {
   var manager = new AjaxManager();
 
   var started = false;
@@ -21,5 +21,62 @@ asyncTest( "Test ajax", function() {
   setTimeout(function() {
     started = true;
     manager.start();
-  }, 200);
+  }, 100);
+});
+
+
+test('test ajax', function(){
+  var manager = new AjaxManager();
+  var expect = {
+    test: true,
+    working: 'test'
+  };
+  manager.ajax(expect);
+
+  deepEqual(manager.queue.pop(), expect, 'enqueued');
+});
+
+
+
+asyncTest('test callback finished', function() {
+  var manager = new AjaxManager();
+
+  var passSuccess = false;
+
+  manager.callbackFinished(function() {
+    ok(LOADER1, 'loader1 ok');
+    ok(passSuccess, 'should pass on success function');
+    this.stop();
+    start();
+  });
+
+  manager.ajax({
+    dataType: 'script',
+    url: 'loader/loader1.js',
+    success: function() {
+      passSuccess = true;
+    },
+    error: function() {
+      ok(false, 'error');
+      start();
+    }
+  });
+  
+  manager.start();
+});
+
+test('test stop', function(){
+  var manager = new AjaxManager();
+  equal(manager.timer, undefined);
+  manager.start();
+  ok(manager.timer != undefined);
+  manager.stop();
+  equal(manager.timer, undefined);
+});
+
+test('test set max', function(){
+  var manager = new AjaxManager();
+  equal(manager.max, 5);
+  manager.newMax(10);
+  equal(manager.max, 10);
 });
